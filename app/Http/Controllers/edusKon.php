e998131bd963543bf4195452a7bcaddcf17ca5cc
literalpass <prakosokun101@gmail.com>
2024-02-15 23:57:45 +0700

    ambil id anjengggg

--- app/Http/Controllers/edusKon.php
+++ app/Http/Controllers/edusKon.php
@@ -2,11 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pegaw;
+use Illuminate\Http\Request;
+
+
 class edusKon extends Controller
 {
     public function index()
     {
         return view('edus');
     }
+
+    public function see($id)
+    {
+        $taek = Pegaw::join('dept', 'pegaw.DEPT_ID', '=', 'dept.DEPT_ID')
+                     ->join('shift', 'pegaw.SHIFT_ID', '=', 'shift.SHIFT_ID')
+                     ->select('pegaw.*', 'dept.*', 'shift.*')
+                     ->get();
+
+        return view('edus', compact('taek'));
+    }
+
 }
 
