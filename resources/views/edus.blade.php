d14e99f56c73928f1733d531849027cef4e4613d
Brian/Prass <97554853+literalprass@users.noreply.github.com>
2024-03-17 04:04:35 +0700

    mastering view (all) (#5)

--- resources/views/edus.blade.php
+++ resources/views/edus.blade.php
@@ -8,46 +8,56 @@
     <title>Employee's detail</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/starter.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/edit.css') }}">
   </head>
-  <body>
+  <body class="bauk">
 
-    <div class="container">
-        <button class="btn btn-dark">
-            <a href="{{ url('/dash') }}" class="nostyle">Back</a>
-        </button>
-
-
-  <form method="POST" action="{{ route('up', ['id' => $taek->id]) }}">
-          @csrf
-          @method('PATCH')
-
-        <div class="mb-4">
-          <input type="input" class="form-control" name="v1" value="{{$taek->nama}}">
+    <div class="card iyh bg-dark text-light halah">
+      <div class="card-body">
+        <div class="container">
+          <div class="cihuy">
+          <label class="todd">Update data {{$taek->nama}}</label>
+            <button class="btn btn-warning mb-3">
+              <a href="{{ url('/dash') }}" class="reng">Back</a>
+            </button>
+          </div>
+          
+          <hr class="wohh rounded">
+  <br>
+    <form method="POST" action="{{ route('up', ['id' => $taek->id]) }}">
+            @csrf
+            @method('PATCH')
+  
+          <div class="mb-4">
+            <input type="input" class="form-control" name="v1" value="{{$taek->nama}}">
+        </div>
+        <div class="input-group mb-4">
+            <label class="input-group-text" for="inputGroupSelect01">Departement</label>
+            <select class="form-select" id="inputGroupSelect01" name="v2">
+              <option selected value="{{$taek->DEPT_ID}}">{{$taek->detil_dept}}</option>
+              <option value="1001" >Illlustrator</option>
+              <option value="1002">Developer</option>
+              <option value="1003">Human resource</option>
+            </select>
+        </div>
+        <div class="input-group mb-4">
+            <label class="input-group-text" for="inputGroupSelect01">Shift</label>
+            <select class="form-select" id="inputGroupSelect01" name="v3">
+              <option selected value="{{$taek->SHIFT_ID}}">{{$taek->ket}}</option>
+              <option value="1">Pagi</option>
+              <option value="2">Siang</option>
+              <option value="3">Malam</option>
+            </select>
+        </div>
+        <br><br>
+        
+        <input type="submit" value="Update" class="btn btn-primary yh">
+        <input type="reset" value="Cancel" class="btn btn-danger yh">
+  
       </div>
-      <div class="input-group mb-4">
-          <label class="input-group-text" for="inputGroupSelect01">Departement</label>
-          <select class="form-select" id="inputGroupSelect01" name="v2">
-            <option selected value="{{$taek->DEPT_ID}}">{{$taek->detil_dept}}</option>
-            <option value="1001" >Illlustrator</option>
-            <option value="1002">Developer</option>
-            <option value="1003">Human resource</option>
-          </select>
+    </form>
       </div>
-      <div class="input-group mb-4">
-          <label class="input-group-text" for="inputGroupSelect01">Shift</label>
-          <select class="form-select" id="inputGroupSelect01" name="v3">
-            <option selected value="{{$taek->SHIFT_ID}}">{{$taek->ket}}</option>
-            <option value="1">Pagi</option>
-            <option value="2">Siang</option>
-            <option value="3">Malam</option>
-          </select>
-      </div>
-
-      <input type="reset" value="Cancel" class="btn btn-danger">
-      <input type="submit" value="Update" class="btn btn-primary">
-
     </div>
-  </form>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
