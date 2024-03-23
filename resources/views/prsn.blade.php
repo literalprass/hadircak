c35c7acc50d9ec9c55c8e3a3397a8553840b646f
literalpass <prakosokun101@gmail.com>
2024-03-24 02:19:28 +0700

    install tailwind

--- resources/views/prsn.blade.php
+++ resources/views/prsn.blade.php
@@ -9,6 +9,7 @@ echo $plg;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>HAI</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
     <a href="{{url('/dash')}}">
