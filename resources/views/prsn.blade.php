cce7f4c71ddaacf71b5634135b8a9a3fa32dc604
literalpass <prakosokun101@gmail.com>
2024-03-24 05:02:04 +0700

    install flowbite

--- resources/views/prsn.blade.php
+++ resources/views/prsn.blade.php
@@ -9,22 +9,25 @@ echo $plg;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>HAI</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css','resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('css/starter.css') }}">
 </head>
-<body>
-    <div>
+<body class="bauk text-white">
+    <div class="flex flex-row">
+
+        <form action="{{url('/prs')}}" method="POST" class="mr-2">
+            @csrf
+            <button type="submit"class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-xl">
+                    Absen
+            </button>
+        </form>
+
         <a href="{{url('/dash')}}">
-            <button class="bg-red-600 hover:bg-red-700 text-white font-bold py-3 px-6 rounded-lg">
+            <button class="bg-red-600 hover:bg-red-700 text-white font-bold py-3 px-6 rounded-xl">
                 back
             </button>
-            </a>
-        
-            <form action="{{url('/prs')}}" method="POST">
-            @csrf
-                <button type="submit"class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg">
-                    Absen
-                </button>
-            </form>
+        </a>
+
     </div>
 </body>
 </html>
